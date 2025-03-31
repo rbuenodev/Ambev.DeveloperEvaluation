5171e4510ff5136d6e3d56fa94a767b02ee4cd09
richard augusto bueno <richard_bueno@ymail.com>
2025-03-30 21:39:28 -0300

    feat: adding functional tests

---
+++ tests/Ambev.DeveloperEvaluation.Functional/Carts/Commands/CreateCartCommandTest.cs
@@ -0,0 +1,26 @@
+﻿using Ambev.DeveloperEvaluation.Application.Carts.CreateCart;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Functional.Carts.Commands
+{
+    [Collection("Database")]
+    public class CreateCartCommandTests : DatabaseTestBase
+    {
+        public CreateCartCommandTests(DatabaseFixture fixture) : base(fixture)
+        {
+        }
+        [Fact]
+        public async Task ShouldCreateValidCart_WhenCommandIsValid()
+        {
+            var command = new CreateCartCommand { Branch = Domain.Enums.Branch.America, SaleNumber = "xx123", UserId = Guid.Parse("ee0ff89b-f80d-495c-b7af-437be2f5b306") };
+            var validationResult = command.Validate();
+
+            Assert.NotNull(validationResult);
+            Assert.True(validationResult.IsValid);
+
+            var response = await SendAsync(command);
+            Assert.NotNull(response);
+            Assert.False(string.IsNullOrEmpty(response.Id.ToString()));
+        }
+    }
+}
---
+++ tests/Ambev.DeveloperEvaluation.Functional/DatabaseTestBase.cs
@@ -0,0 +1,95 @@
+﻿using Ambev.DeveloperEvaluation.ORM;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Ambev.DeveloperEvaluation.Functional
+{
+    [CollectionDefinition("Database")]
+    public class DatabaseCollection : ICollectionFixture<DatabaseFixture> { }
+    [Collection("Database")]
+    public abstract class DatabaseTestBase : IAsyncLifetime
+    {
+        private readonly DatabaseFixture _fixture;
+        private IServiceScope _testScope;
+
+        protected DefaultContext DbContext { get; private set; }
+        protected IMediator Mediator { get; private set; }
+        protected IServiceProvider Services { get; private set; }
+        protected DatabaseTestBase(DatabaseFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        public async Task InitializeAsync()
+        {
+            _testScope = _fixture.CreateScope();
+            Services = _testScope.ServiceProvider;
+            DbContext = Services.GetRequiredService<DefaultContext>();
+            Mediator = Services.GetRequiredService<IMediator>();
+            await SeedDatabaseAsync();
+        }
+
+        public async Task DisposeAsync()
+        {
+            if (DbContext != null)
+            {
+                await DbContext.DisposeAsync();
+            }
+
+            _testScope?.Dispose();
+        }
+
+        protected virtual Task SeedDatabaseAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+        {
+            return await ExecuteInTransactionAsync(async () =>
+            {
+                return await Mediator.Send(request);
+            });
+        }
+
+        protected async Task SendAsync(IRequest request)
+        {
+            await ExecuteInTransactionAsync(async () =>
+            {
+                await Mediator.Send(request);
+            });
+        }
+
+        protected async Task<T> ExecuteInTransactionAsync<T>(Func<Task<T>> testAction)
+        {
+            await using var transaction = await DbContext.Database.BeginTransactionAsync();
+            try
+            {
+                var result = await testAction();
+                await transaction.RollbackAsync();
+                return result;
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+
+        protected async Task ExecuteInTransactionAsync(Func<Task> testAction)
+        {
+            await using var transaction = await DbContext.Database.BeginTransactionAsync();
+            try
+            {
+                await testAction();
+                await transaction.RollbackAsync();
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+    }
+}
